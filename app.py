853f02bfa557d7a159f90433e4ff9c4b0d7f0bd5
ghstudent1 <ghstudent@gmail.com>
2019-04-29 21:39:15 +0100

    add modif to jenkinsfile

--- app.py
+++ app.py
@@ -12,4 +12,10 @@ def hello_user(username):
     return 'Why Hello %s!\n' % username
 
 if __name__ == '__main__':
+    ############# Add these lines #############
+    import xmlrunner
+    runner = xmlrunner.XMLTestRunner(output='test-reports')
+    unittest.main(testRunner=runner)
+    ###########################################
+    unittest.main()
     app.run(host='0.0.0.0')     # open for everyone
